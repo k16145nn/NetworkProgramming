a2d3a1b8f5fd82b0450bdc9015c200c2fe682419
4420_長尾 直行 <k16145nn>
2019-06-28 10:36:00 +0900

    20190628

--- www/server.c
+++ www/server.c
@@ -19,8 +19,19 @@ void DieWithError(char *errorMessage){
 void commun(int sock){
 	char buf[BUF_SIZE];
 	int len_r;
-	if((len_r=recv(sock,buf,BUF_SIZE,0))<=0)DieWithError("recv()faile");
-	buf[len_r]='\0';
+	char * adr;
+	adr = strstr(buf,("\r\n\n\n");
+	while((len_r=recv(sock,buf,BUF_SIZE,0))>0){
+		buf[len_r]='\0';
+		printf("%s\n",buf);
+		if(strstr(buf,"\r\n\r\n")){
+			printf(#received HTTP request.");
+			break;
+		}
+	}
+		if(len_r <=0)DieWithError("recv()faile");
+		printf("received HTTP Request,\n");
+	}
 	printf("%s\n",buf);
 	if(send(sock,buf,strlen(buf),0)!=strlen(buf))DieWithError("tanaka()faile");
 	
