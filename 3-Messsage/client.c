ef91ebcb6610e4a04d7ed3da9681ba18d8d9a3da
k16145nn <k16145nn@apps.kct.ac.jp>
2019-10-17 16:05:43 +0900

    tanaja

--- 3-Messsage/client.c
+++ 3-Messsage/client.c
@@ -10,21 +10,85 @@
 
 void DieWithError(char *);
 int prepare_client_socket(char *, int);
-void my_scanf
+void my_scanf(char *, int);
+void commun(int);
 
-    int
-    main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     if (argc != 3)
-        DieWithError("usage: ./client ip_address port")
+        DieWithError("usage: ./client ip_address port");
 
-            void
-            DieWithError(char *errorMessage)
-        {
-            perror(errorMessage);
-            exit(1);
-        }
+    int sock = prepare_client_socket(argv[1], atoi(argv[2]));
 
-    int prepare_client_socket(char *)
+    commun(sock);
+
+    close(sock);
+
+    return 0;
 }
+void DieWithError(char *errorMessage)
+{
+    perror(errorMessage);
+    exit(1);
+}
+
+int prepare_client_socket(char *ipassr, int port)
+{
+    int sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+        DieWithError("socket() failed");
+
+    struct sockaddr_in target;
+    target.sin_family = AF_INET;
+    target.sin_addr.s_addr = htonl(INADDR_ANY);
+    target.sin_port = htons(port);
+    if (connect(sock, (struct sockaddr *)&target, sizeof(target)) < 0)
+        DieWithError("connect() failed");
+
+    return sock;
+}
+
+void my_scanf(char *buf, int num_letter)
+{
+
+    char format[20];
+    sprintf(format, "%s%d%s", "%", num_letter, "s%*[^\n]");
+    scanf(format, buf);
+    getchar();
+}
+void commun(int sock)
+{
+    char cmd[2] = "";
+    char withdraw[MONEY_DIGIT_SIZE + 1];
+    char deposit[MONEY_DIGIT_SIZE + 1];
+    char msg[BUF_SIZE];
+
+    printf("0:引き出し　1:預け入れ 2:残高参照 \n");
+    printf("何をしますか? > ");
+
+    my_scanf(cmd, 1);
+
+    switch (cmd[0])
+    {
+    case '0':
+        printf("引き出す金額を入力してください > ");
+        my_scanf(withdraw, MONEY_DIGIT_SIZE);
+
+        sprintf(msg, "0_%s_", withdraw);
+        break;
+    case '1':
+        printf("預け入れる金額を選んでください > ");
+        my_scanf(deposit, MONEY_DIGIT_SIZE);
+
+        sprintf(msg, "%s_0_", deposit);
+        break;
+    case '2':
+        strcpy(msg, "0_0_");
+        break;
+    default:
+        printf("番号が確認できませんでした.\n");
+        return;
+    }
+    if (send(sock, msg, strlen(msg), 0) != strlen(msg))
+        DieWithError("send()sent a message of unexpected dytes");
 }
--- 3-Messsage/server.c
+++ 3-Messsage/server.c
@@ -11,8 +11,11 @@
 void DieWithError(char *);
 int prepare_server_socket(int);
 void commun(int);
+void read_until_delim(int, char *, char, int);
+int get_current_balance();
+void set_current_balance(int);
 
-int main(int argc, char[])
+int main(int argc, char *argv[])
 {
     struct sockaddr_in clientAddess;
     unsigned int szClientAddr;
@@ -25,7 +28,7 @@ int main(int argc, char[])
     while (1)
     {
         szClientAddr = sizeof(clientAddess);
-        cliSock = accept(servSock, (struct sockaddr *)&clientAddess, &szClientaddr);
+        cliSock = accept(servSock, (struct sockaddr *)&clientAddess, &szClientAddr);
 
         commun(cliSock);
 
@@ -43,9 +46,64 @@ void DieWithError(char *errorMessage)
 
 int prepare_server_socket(int port)
 {
-    int servSock = socket(PF_INET, SOCK_STREA, 0);
+    int servSock = socket(PF_INET, SOCK_STREAM, 0);
     if (servSock < 0)
         DieWithError("socket() failed");
 
     struct sockaddr_in servAddress;
+    servAddress.sin_family = AF_INET;
+    servAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddress.sin_port = htons(port);
+
+    bind(servSock, (struct sockaddr *)&servAddress, sizeof(servAddress));
+
+    return servSock;
+}
+
+void read_until_delim(int sock, char *buf, char delimiter, int max_length)
+{
+    int len_r = 0;
+    int index_letter = 0;
+
+    while (index_letter < max_length - 1)
+    {
+        if ((len_r = recv(sock, buf + index_letter, 1, 0)) <= 0)
+        {
+
+            printf("接続が切れました\n");
+            return;
+        }
+        if (buf[index_letter] == delimiter)
+            break;
+        else
+            index_letter++;
+    }
+    buf[index_letter] = '\0';
+}
+
+int get_current_balance()
+{
+    return 1000000;
+}
+
+void set_current_balance(int new_balance)
+{
+    return;
+}
+
+void commun(int sock)
+{
+    char buf[BUF_SIZE];
+    int balance = get_current_balance();
+
+    read_until_delim(sock, buf, '_', BUF_SIZE);
+    balance += atoi(buf);
+    read_until_delim(sock, buf, '_', BUF_SIZE);
+    balance -= atoi(buf);
+
+    set_current_balance(balance);
+
+    sprintf(buf, "%d_", balance);
+    if (send(sock, buf, strlen(buf), 0) != strlen(buf))
+        DieWithError("send() sent amessage of unexpected bytes");
 }
